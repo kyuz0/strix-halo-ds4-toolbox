84a580d8c8b03602143a9f2a14183f3b45e3bbc1
Donato Capitella <donato.capitella@reversec.com>
2026-06-16 19:24:33 +0100

    Revert "add experimental gfx1201 target"
    This reverts commit 9646c268f3980a7fc270572befa24b5daa8c4040.

--- refresh-toolboxes.sh
+++ refresh-toolboxes.sh
@@ -7,7 +7,6 @@ declare -A TOOLBOXES
 
 TOOLBOXES["ds4-rocm-7.2.4"]="docker.io/kyuz0/strix-halo-ds4-toolbox:rocm-7.2.4 --device /dev/dri --device /dev/kfd --group-add video --group-add render --group-add sudo --security-opt seccomp=unconfined"
 TOOLBOXES["ds4-multi-node-rocm-7.2.4"]="docker.io/kyuz0/strix-halo-ds4-toolbox:multi-node-rocm-7.2.4 --device /dev/dri --device /dev/kfd --group-add video --group-add render --group-add sudo --security-opt seccomp=unconfined"
-TOOLBOXES["ds4-gfx1201-rocm-7.2.4"]="docker.io/kyuz0/strix-halo-ds4-toolbox:gfx1201-rocm-7.2.4 --device /dev/dri --device /dev/kfd --group-add video --group-add render --group-add sudo --security-opt seccomp=unconfined"
 
 function usage() {
   echo "Usage: $0 [all|toolbox-name1 toolbox-name2 ...]"
